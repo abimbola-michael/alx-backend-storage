861e18e96a9d7df2a1a00416b974106b2df42392
Abimbola Michael <abimbolamichael100.com>
2024-01-18 19:16:19 +0100

    Correction

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,22 +6,18 @@
 -- user_id, a users.id value (you can assume user_id is linked to an existing users)
 
 DELIMITER $$ ;
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE total_score FLOAT;
     DECLARE total_projects INT;
-
     -- Calculate total score and count of projects
     SELECT SUM(score), COUNT(DISTINCT project_id)
     INTO total_score, total_projects
     FROM corrections
-    WHERE user_id = user_id;
-
+    WHERE user_id=user_id;
     -- Update average score for the user
     UPDATE users
-    SET average_score = IFNULL(total_score / total_projects, 0)
-    WHERE id = user_id;
+    SET average_score=IFNULL(total_score / total_projects, 0)
+    WHERE id=user_id;
 END $$ ;
 DELIMITER ;
